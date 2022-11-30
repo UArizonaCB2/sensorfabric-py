2bc4cb1e624f42a12052c115c6efcfa249a11076
Shravan Aras <123.shravan@gmail.com>
2022-11-29 19:46:06 -0700

    Added long description for python package

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+long_description = 'Python library for UA Sensor Fabric'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='sensorfabric',
-    version='0.1.1',
+    version='0.1.2',
     description='Python library for UA Sensor Fabric',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     url='https://github.com/UArizonaCB2/sensorfabric-py.git',
     author="Shravan Aras",
