50ea2d790f740e6bd5d36170721c1905a5e7e0b2
Shravan Aras <123.shravan@gmail.com>
2024-03-20 15:55:30 -0700

    Massive Flattenin Speedup!
    The code that flattens the JSON file has been optmized.
    Each 10 min file now takes < 1.5seconds instead of 60 seconds.
    Hurray! This was done by passing dictionary objects during recursion
    levels instead of passing full pandas dataframes.

--- sensorfabric/json/Flatten.py
+++ sensorfabric/json/Flatten.py
@@ -10,7 +10,7 @@ import json
 import pandas
 import numpy as np
 
-def flatten(json_data : json, base_path='', sep='_', fill=True) -> pandas.DataFrame:
+def flatten(json_data : json, base_path='', sep='_', fill=True) -> dict:
     """
     Description
     -----------
@@ -29,43 +29,82 @@ def flatten(json_data : json, base_path='', sep='_', fill=True) -> pandas.DataFr
 
     Returns
     -------
-    Returns a pandas dataframe.
+    Returns a dictionary object with the keys as columns.
     """
 
     # Append a key to the base path.
     appendPath = lambda p, k : k if len(p) <= 0 else p+sep+k
 
     if type(json_data) == type(dict()):
-        frame = pandas.DataFrame()
+        #frame = pandas.DataFrame()
+        frame = {}
         for k in json_data.keys():
-            f = flatten(json_data[k], appendPath(base_path, k), sep)
+            f = flatten(json_data[k], appendPath(base_path, k), sep, fill)
             # Special case : If we are concating a frame of shape 1xm with nxp
             # then the based on the `fill` argument we will expand the 1xm frame to nxm
             # frame before concatinating. Hence filling the empty spaces and causing
             # redandant data.
             if fill:
-                if frame.shape[0] == 1 and f.shape[0] > 1:
+                if len(list(frame.items())) < len(list(f.items())):
                     frame = _filler(frame, f)
-                elif f.shape[0] == 1 and frame.shape[0] > 1:
+                elif len(list(frame.items())) > len(list(f.items())):
                     f = _filler(f, frame)
             # These need to be concatinated along the column axis.
-            frame = pandas.concat([frame, f], axis=1)
+            #frame = pandas.concat([frame, f], axis=1)
+            frame = _concat(frame, f)
         return frame
     elif type(json_data) == type(list()):
         # For each element in the list recursively call the flattener and append to existing frame.
-        frame = pandas.DataFrame()
+        #frame = pandas.DataFrame()
+        frame = {}
         for ele in json_data:
-            f = flatten(ele, base_path, sep)
+            f = flatten(ele, base_path, sep, fill)
             # These need to be concatinated along the index axis.
-            frame = pandas.concat([frame, f], axis=0, ignore_index=True)
+            #frame = pandas.concat([frame, f], axis=0, ignore_index=True)
+            frame = _concat(frame, f)
         return frame
     else:
-        return pandas.DataFrame({base_path:json_data}, index=[0])
+        #return pandas.DataFrame({base_path:json_data}, index=[0])
+        return {base_path:json_data}
 
-def _filler(small : pandas.DataFrame, big : pandas.DataFrame) -> pandas.DataFrame:
-    cols = small.columns
-    map = {}
-    for c in cols:
-        map[c] = [small.at[0, c]] * big.shape[0]
+def _concat(dictA, dictB):
+    """
+    Merges the contents of dictionary B into A
+    """
+    for k in dictB.keys():
+        if not(k in dictA):
+            dictA[k] = []
+        if type(dictB[k]) == type(list()):
+            dictA[k].extend(dictB[k])
+        else:
+            dictA[k].append(dictB[k])
 
-    return pandas.DataFrame(map)
+    return dictA
+
+def _isSimpleJson(obj):
+    for k in obj.keys():
+        if type(obj[k]) == type(dict()) or type(obj[k]) == type(list()):
+            return False
+    return True
+
+    
+def _filler(small : dict, big : dict) -> dict:
+    """
+    Extend the smaller dictionary to match the big on.
+    """
+
+    targetCount = 0
+    for k in big.keys():
+        if type(big[k]) == type(list()):
+            if len(big[k]) > targetCount:
+                targetCount = len(big[k])
+
+    for k in small.keys():
+        if not(type(small[k]) == type(list())):
+            small[k] = [small[k]] # If it is not a list we make it into a list.
+        if len(small[k]) < targetCount:
+            lastElement = small[k][-1]
+            for i in range(len(small[k]), targetCount):
+                small[k].append(lastElement)
+
+    return small
