6b9bd415b22a72ee25ed9dcdcaa49c938f1011e8
Nick Eddy <eddy.nicholas@gmail.com>
2025-06-02 16:55:58 -0700

    feat: add templates.py to sensorfabric module

---
+++ sensorfabric/templates.py
@@ -0,0 +1,390 @@
+### json data from ultrahuman API:
+# 
+# [
+#     {
+#         "type": "hr",
+#         "object": {
+#             "day_start_timestamp": 1671739200,
+#             "title": "Heart Rate",
+#             "values": [
+#                 {
+#                     "value": 81,
+#                     "timestamp": 1671739294
+#                 },
+#                 {
+#                     "value": 94,
+#                     "timestamp": 1671739586
+#                 },
+#                 {
+#                     "value": 88,
+#                     "timestamp": 1671739887
+#                 },
+#                 {
+#                     "value": 80,
+#                     "timestamp": 1671740155
+#                 },
+#                 {
+#                     "value": 76,
+#                     "timestamp": 1671740447
+#                 },
+#                 {
+#                     "value": 84,
+#                     "timestamp": 1671740750
+#                 },
+#                 {
+#                     "value": 79,
+#                     "timestamp": 1671741034
+#                 },
+#                 {
+#                     "value": 74,
+#                     "timestamp": 1671741342
+#                 },
+#                 {
+#                     "value": 84,
+#                     "timestamp": 1671741623
+#                 },
+#                 {
+#                     "value": 78,
+#                     "timestamp": 1671741911
+#                 },
+#                 {
+#                     "value": 85,
+#                     "timestamp": 1671742204
+#                 },
+#                 {
+#                     "value": 80,
+#                     "timestamp": 1671742497
+#                 },
+#                 {
+#                     "value": 93,
+#                     "timestamp": 1671742812
+#                 }
+#             ],
+#             "last_reading": 78,
+#             "unit": "BPM"
+#         }
+#     },
+#     {
+#         "type": "temp",
+#         "object": {
+#             "day_start_timestamp": 1671739200,
+#             "title": "Skin Temperature",
+#             "values": [
+#                 {
+#                     "value": 32.84204864501953,
+#                     "timestamp": 1671739393
+#                 },
+#                 {
+#                     "value": 31.783939361572266,
+#                     "timestamp": 1671739686
+#                 },
+#                 {
+#                     "value": 32.68800354003906,
+#                     "timestamp": 1671739979
+#                 },
+#                 {
+#                     "value": 33.4249153137207,
+#                     "timestamp": 1671740272
+#                 },
+#                 {
+#                     "value": 30.226356506347656,
+#                     "timestamp": 1671740565
+#                 }
+#             ],
+#             "last_reading": 33,
+#             "unit": "°C"
+#         }
+#     },
+#     {
+#         "type": "hrv",
+#         "object": {
+#             "day_start_timestamp": 1671739200,
+#             "title": "HRV",
+#             "values": [
+#                 {
+#                     "value": 100,
+#                     "timestamp": 1671739586
+#                 },
+#                 {
+#                     "value": 33,
+#                     "timestamp": 1671740155
+#                 },
+#                 {
+#                     "value": 34,
+#                     "timestamp": 1671740447
+#                 },
+#                 {
+#                     "value": 49,
+#                     "timestamp": 1671741034
+#                 },
+#                 {
+#                     "value": 39,
+#                     "timestamp": 1671741342
+#                 }
+#             ],
+#             "subtitle": "Average",
+#             "avg": 32,
+#             "trend_title": "-14",
+#             "trend_direction": "negative"
+#         }
+#     },
+#     {
+#         "type": "steps",
+#         "object": {
+#             "day_start_timestamp": 1671739200,
+#             "values": [
+#                 {
+#                     "value": 114.0,
+#                     "timestamp": 1671739393
+#                 },
+#                 {
+#                     "value": 25.0,
+#                     "timestamp": 1671739686
+#                 },
+#                 {
+#                     "value": 0.0,
+#                     "timestamp": 1671739979
+#                 },
+#                 {
+#                     "value": 0.0,
+#                     "timestamp": 1671740272
+#                 },
+#                 {
+#                     "value": 59.0,
+#                     "timestamp": 1671740565
+#                 }
+#             ],
+#             "subtitle": "Average",
+#             "avg": 28.305084745762713,
+#             "trend_title": "+5",
+#             "trend_direction": "positive"
+#         }
+#     },
+#     {
+#         "type": "motion",
+#         "object": {
+#             "day_start_timestamp": 1671739200,
+#             "values": [
+#                 {
+#                     "value": 104.0,
+#                     "timestamp": 1671739393
+#                 },
+#                 {
+#                     "value": 74.0,
+#                     "timestamp": 1671739686
+#                 },
+#                 {
+#                     "value": 15.0,
+#                     "timestamp": 1671739979
+#                 },
+#                 {
+#                     "value": 12.0,
+#                     "timestamp": 1671740272
+#                 },
+#                 {
+#                     "value": 63.0,
+#                     "timestamp": 1671740565
+#                 },
+#                 {
+#                     "value": 42.0,
+#                     "timestamp": 1671740858
+#                 }
+#             ],
+#             "sleep_graph": {
+#                 "title": "Sleep Stages",
+#                 "data": [
+#                     {
+#                         "start": 1671744060,
+#                         "end": 1671744360,
+#                         "type": "awake"
+#                     },
+#                     {
+#                         "start": 1671744360,
+#                         "end": 1671744960,
+#                         "type": "deep_sleep"
+#                     },
+#                     {
+#                         "start": 1671744960,
+#                         "end": 1671745200,
+#                         "type": "light_sleep"
+#                     },
+#                     {
+#                         "start": 1671745200,
+#                         "end": 1671745500,
+#                         "type": "awake"
+#                     },
+#                     {
+#                         "start": 1671745500,
+#                         "end": 1671747540,
+#                         "type": "light_sleep"
+#                     },
+#                     {
+#                         "start": 1671747540,
+#                         "end": 1671748740,
+#                         "type": "deep_sleep"
+#                     },
+#                     {
+#                         "start": 1671748740,
+#                         "end": 1671749340,
+#                         "type": "light_sleep"
+#                     },
+#                     {
+#                         "start": 1671749340,
+#                         "end": 1671750180,
+#                         "type": "deep_sleep"
+#                     },
+#                     {
+#                         "start": 1671750180,
+#                         "end": 1671753720,
+#                         "type": "light_sleep"
+#                     }
+#                 ]
+#             },
+#             "hr_graph": {
+#                 "title": "HR",
+#                 "data": [
+#                     {
+#                         "timestamp": 1671744060,
+#                         "value": 80.5
+#                     },
+#                     {
+#                         "timestamp": 1671744360,
+#                         "value": 77.5
+#                     },
+#                     {
+#                         "timestamp": 1671744660,
+#                         "value": 75.0
+#                     },
+#                     {
+#                         "timestamp": 1671744960,
+#                         "value": 78.0
+#                     },
+#                     {
+#                         "timestamp": 1671745200,
+#                         "value": 80.5
+#                     },
+#                     {
+#                         "timestamp": 1671745500,
+#                         "value": 78.0
+#                     },
+#                     {
+#                         "timestamp": 1671745800,
+#                         "value": 74.5
+#                     },
+#                     {
+#                         "timestamp": 1671746100,
+#                         "value": 72.5
+#                     },
+#                     {
+#                         "timestamp": 1671746400,
+#                         "value": 70.0
+#                     },
+#                     {
+#                         "timestamp": 1671746700,
+#                         "value": 69.0
+#                     },
+#                     {
+#                         "timestamp": 1671747000,
+#                         "value": 70.5
+#                     },
+#                     {
+#                         "timestamp": 1671747300,
+#                         "value": 70.0
+#                     },
+#                     {
+#                         "timestamp": 1671747540,
+#                         "value": 70.0
+#                     },
+#                     {
+#                         "timestamp": 1671747840,
+#                         "value": 70.0
+#                     }
+#                 ]
+#             },
+#             "hrv_graph": {
+#                 "title": "HRV",
+#                 "data": [
+#                     {
+#                         "timestamp": 1671744060,
+#                         "value": 14.5
+#                     },
+#                     {
+#                         "timestamp": 1671744360,
+#                         "value": 17.0
+#                     },
+#                     {
+#                         "timestamp": 1671744660,
+#                         "value": 14.5
+#                     },
+#                     {
+#                         "timestamp": 1671744960,
+#                         "value": 25.5
+#                     },
+#                     {
+#                         "timestamp": 1671745200,
+#                         "value": 32.75
+#                     },
+#                     {
+#                         "timestamp": 1671745500,
+#                         "value": 28.0
+#                     },
+#                     {
+#                         "timestamp": 1671745800,
+#                         "value": 12.5
+#                     }
+#                 ]
+#             },
+#             "summary": [
+#                 {
+#                     "title": "Sleep Efficiency",
+#                     "state": "warning",
+#                     "state_title": "Needs attention",
+#                     "score": 65,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=sleep_efficiency_sleep"
+#                 },
+#                 {
+#                     "title": "Total Sleep",
+#                     "state": "good",
+#                     "state_title": "Good",
+#                     "score": 79,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=total_sleep_sleep"
+#                 },
+#                 {
+#                     "title": "Temperature",
+#                     "state": "optimal",
+#                     "state_title": "Optimal",
+#                     "score": 98,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=temperature_sleep"
+#                 },
+#                 {
+#                     "title": "Restfulness",
+#                     "state": "good",
+#                     "state_title": "Good",
+#                     "score": 76.75,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=restfulness_sleep"
+#                 },
+#                 {
+#                     "title": "HRV Form",
+#                     "state": "warning",
+#                     "state_title": "Needs attention",
+#                     "score": 50,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=hrv_sleep"
+#                 },
+#                 {
+#                     "title": "Timing",
+#                     "state": "warning",
+#                     "state_title": "Needs attention",
+#                     "score": 40,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=timing_sleep"
+#                 },
+#                 {
+#                     "title": "Restoration Time",
+#                     "state": "warning",
+#                     "state_title": "Needs attention",
+#                     "score": 60.47222222222222,
+#                     "education_modal_deeplink": "ultrahuman://educationModal?id=restoration_time"
+#                 }
+#             ]
+#         }
+#     } // End of motion object
+# ]
